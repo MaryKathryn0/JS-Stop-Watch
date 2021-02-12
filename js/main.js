feacd4f34a009c64866542aa4b3ce2486c97207b
Mary Kathryn <msmarykathrynholt@gmail.com>
2021-02-12 15:47:31 -0600

    added click listener for toggle button

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ var timer = document.getElementById('timer');
 var toggleBtn = document.getElementById('toggle');
 var resetBtn = document.getElementById('reset');
 
+//add a click listener for toggle button, so that when we click will get function
+toggleBtn.addEventListener('click', function() {
+    //if watch on then stop, else start
+    if (watch.isOn) {
+        watch.stop();
+    } else {
+        watch.start();
+    }
+});

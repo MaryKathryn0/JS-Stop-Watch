23473c68599e05d2e14db88fe0d294d31d11a335
Mary Kathryn <msmarykathrynholt@gmail.com>
2021-02-11 18:20:46 -0600

    Created constructor function, Stopwatch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+
+// Constructor Function():is a function we use to create an OBJECT
+// Stopwatch() : this is the function we will be working with
+function Stopwatch() {
+    // The time is the current time in milliseconds(will format later)
+    var time;
+    //
+    var interval;
+    var offset;
+
+    //Add a value set to false; will be if stopwatch isnt currently running
+    this.isOn= false;
+    //first added start function
+    this.start= function() {};
+    // then will need to add stop, reset, 
+    this.stop= function() {};
+    this.reset= function() {};
+}
+// thru new operator using 'new' Keyword- anything that happens to 'this' above
+var watch = new Stopwatch();
+watch.start();
+
+

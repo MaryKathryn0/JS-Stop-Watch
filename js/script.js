b8ea4d7484a089b0c72707430708a7374850a574
Mary Kathryn <msmarykathrynholt@gmail.com>
2021-02-11 18:54:02 -0600

    add private varibles & functions to Stopwatch()

--- js/script.js
+++ js/script.js
@@ -2,15 +2,27 @@
 // Constructor Function():is a function we use to create an OBJECT
 // Stopwatch() : this is the function we will be working with
 function Stopwatch() {
+    // Create Private Variables:
     // The time is the current time in milliseconds(will format later)
-    var time;
-    //
+    var time = 0;
+    // Interval variable is when hit start it will run update function
+    // and need to save for stop function
     var interval;
+    // offset variable: going to set this variable to date.now to calculate how much time has passed
     var offset;
+
+    //Now add private functions
+    //Update is what the interval will constantly keep going
+    function update() {}
+    //delta is to calculate how much time has passed
+    function delta() {}
+    //timeFor is to convert from milliseconds
+    function timeFormatter() {}
 
     //Add a value set to false; will be if stopwatch isnt currently running
     this.isOn= false;
-    //first added start function
+
+    //first added start function- this functions
     this.start= function() {};
     // then will need to add stop, reset, 
     this.stop= function() {};
